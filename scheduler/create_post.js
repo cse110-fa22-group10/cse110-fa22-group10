621f47093d722eb4f3f91b30bdc04f64a7a39692
apesovilella <apesovilella@ucsd.edu>
2022-11-14 14:18:46 -0800

    Constraints for Text and Image completed

--- scheduler/create_post.js
+++ scheduler/create_post.js
@@ -3,30 +3,55 @@
 // need a picture, etc..
 const postDescription = document.getElementById('desc-input');
 const postTag = document.getElementById('tag');
+const imageInput = document.getElementById('image-input');
 const submitButton = document.getElementById('submit');
+
+
 
 // Limit the amount of characters permited for every type of posts
 function constraints() {
     let selectedTag = postTag.selectedOptions[0];
-
     // Facebook 63,206char max
-    if(selectedTag == postTag.options[0]) {
+    if (selectedTag == postTag.options[0]) {
         postDescription.maxLength = 63206;
     }
     // Twitter 280char max
-    if(selectedTag == postTag.options[1]) {
+    if (selectedTag == postTag.options[1]) {
         postDescription.maxLength = 280;
     }
-    // Instagram 2,200char max
-    if(selectedTag == postTag.options[2]) {
+    // Instagram 2,200char max and check if there is an image uploaded
+    if (selectedTag == postTag.options[2]) {
         postDescription.maxLength = 2200;
+        checkImage();
     }
 }
 
-// We set the maxLength for Facebook
-postDescription.maxLength = 63206;
+
+// Function called when an image is needed in
+// the image-input to allow the submit button to work
+function checkImage() {
+    if (imageInput.files.length == 0) {
+        submitButton.disabled = true;
+    }
+    else {
+        submitButton.disabled = false;
+    }
+}
+
+// Function called when clicking the submit button to check
+// if the text constraints are respected
+function checkText() {
+    if (postDescription.value.length > postDescription.maxLength) {
+        submitButton.disabled = true;
+        alert("Too many characters!");
+        setTimeout(() => {submitButton.disabled = false}, 1000);
+    }
+}
+
 // Event listeners
 postTag.addEventListener('change', constraints);
+imageInput.addEventListener('change', checkImage);
+submitButton.addEventListener('click', checkText);
 
 // TODO: OnSubmit - store the formdata into localStorage to wherever we want
 // it to be stored. Should also store the time and date of when the post should
