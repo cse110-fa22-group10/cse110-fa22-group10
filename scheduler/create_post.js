635dafc67a929c40ae53715262cf4bd094d3c824
Charles Young <cjyoung2001@yahoo.com>
2022-11-14 15:32:05 -0800

    move files back to sc

---
+++ scheduler/create_post.js
@@ -0,0 +1,7 @@
+// TODO: Create way to set constraints in the post based on what platform
+// the post is for. i.e. Twitter posts need a character limit, Insta posts
+// need a picture, etc..
+
+// TODO: OnSubmit - store the formdata into localStorage to wherever we want
+// it to be stored. Should also store the time and date of when the post should
+// be posted
