4fd4c93705098f16a66a2fc179fbd3c9b76f2b4a
Buwei Wu <buwu@ucsd.edu>
2022-11-16 16:06:05 -0800

    Create functions in index.js finished

--- scheduler/index.js
+++ scheduler/index.js
@@ -1,11 +1,11 @@
 // TODO: Once the facebook button is clicked, move to the creation page of the Facebook posts
 function createFb(){
-    window.location.replace("./create_post/createFb.html");
+    window.location.replace("create_post/createFb.html");
 }
 
 // TODO: Once the instagram button is clicked, move to the creation page of the Instagram posts
 function createIns(){
-    window.location.replace("./create_post/createIns.html");
+    window.location.replace("..create_post/createIns.html");
 
 }
 
