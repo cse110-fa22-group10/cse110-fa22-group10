bad2ca9b0a9f7a943a634977a86b37b4bfcae947
Alexander G. Arias <agarias@ucsd.edu>
2022-12-02 23:24:44 -0800

    event listener for delete img data for createIns

--- scheduler/create_post/createIns.js
+++ scheduler/create_post/createIns.js
@@ -79,6 +79,15 @@ submitButton.addEventListener('click', () => {
     savePostsToStorage(postFromLocal);
 });
 
+// an event listener for the delete image data button in charge of removing images
+// when creating  a post
+deleteImgDataButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    dataUrl = '';
+    imgPreview.innerHTML = '';
+    imageInput.value = '';
+});
+
 /**
  * Reads 'posts' from localStorage and returns an array of
  * all of the posts found (parsed, not in string form). If
